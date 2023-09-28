c71dbe616ec97ee00bcc7f97d8dd56b0ff960ffc
Ankit Kumar <114305963+Ankitkumar1412-debug@users.noreply.github.com>
2023-09-28 11:04:58 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Subarray with 0 sum/subarray-with-0-sum.cpp
@@ -0,0 +1,70 @@
+//{ Driver Code Starts
+// A C++ program to find if there is a zero sum
+// subarray
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    //Complete this function
+    //Function to check whether there is a subarray present with 0-sum or not.
+    bool subArrayExists(int arr[], int n)
+    {
+        //Your code here
+        
+        /*unordered_set<int> hash;
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum+=arr[i];
+            
+            if(sum==0){
+                return true;
+            }
+            // sum hmko mila kya ? Agar h toh
+            if(hash.find(sum)!=hash.end()){
+                return true;
+            }
+            //Agar nai mila toh hm sum ko hash mai insert kr denge 
+            hash.insert(sum);
+        }
+        return false;*/
+        
+        unordered_set<int>s;
+        int pre_sum = 0;
+        for(int i = 0; i < n; i++){
+            pre_sum += arr[i];
+            if(s.find(pre_sum)!=s.end())
+                return true;
+            if(pre_sum == 0)
+                return true;
+            s.insert(pre_sum);
+        }
+        return false;
+    }
+};
+
+//{ Driver Code Starts.
+// Driver code
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    int arr[n];
+	    for(int i=0;i<n;i++)
+	    cin>>arr[i];
+	    Solution obj;
+	    	if (obj.subArrayExists(arr, n))
+		cout << "Yes\n";
+	else
+		cout << "No\n";
+	}
+	return 0;
+}
+// } Driver Code Ends
